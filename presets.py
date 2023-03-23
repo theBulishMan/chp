1d64fef8a3b5eae77167b7cec328a8e9212cbcb0
theBulishMan <100390604+theBulishMan@users.noreply.github.com>
2023-03-23 15:38:38 +0800

    Update presets.py

--- presets.py
+++ presets.py
@@ -29,11 +29,6 @@ title = """<h1 align="left" style="min-width:200px; margin-top:0;">川虎ChatGPT
 description = """\
 <div align="center" style="margin:16px 0">
 
-由Bilibili [土川虎虎虎](https://space.bilibili.com/29125536) 和 [明昭MZhao](https://space.bilibili.com/24807452)开发
-
-访问川虎ChatGPT的 [GitHub项目](https://github.com/GaiZhenbiao/ChuanhuChatGPT) 下载最新版脚本
-
-此App使用 `gpt-3.5-turbo` 大语言模型
 </div>
 """
 
